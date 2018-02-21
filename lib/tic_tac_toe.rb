faac22b7492b40e6fbe2084e4fbf1bebf43a9c72
Elana Ryznar <eryzna@gmail.com>
2018-02-21 17:32:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,14 @@ def valid_move?(index)
   end
 end
 
+def turn_count
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
 end
