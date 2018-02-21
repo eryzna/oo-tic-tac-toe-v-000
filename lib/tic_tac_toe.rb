748a2ba8c9558f2c51682fa7493d704485e0d5d8
Elana Ryznar <eryzna@gmail.com>
2018-02-21 17:30:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,15 +19,15 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   def input_to_index(user_input)
   user_input.to_i-1
   end
-  
+
   def move(index, current_player="X")
   @board[index]= current_player
   end
-  
+
   def position_taken?(index)
   @board[index]== "X" || @board[index] == "O"
 end
